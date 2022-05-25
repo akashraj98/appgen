7374cf94748334c406fd0ca3f35f98f7aa63db4f
Tushar Mishra <usheringtushar@gmail.com>
2022-05-25 21:23:08 +0530

    change db on the fly

--- src/create_proj/helpers.py
+++ src/create_proj/helpers.py
@@ -13,19 +13,20 @@ def create_django_angular_project(database):
     proj_name = ''.join(random.choices(string.ascii_lowercase, k=10))
     parent_dir = pathlib.Path.cwd().parent.parent / proj_name
     db_name = ''.join(random.choices(string.ascii_lowercase, k=10))
+    DATABASES = "{opening_brace}'default': {opening_brace}'ENGINE': 'django.db.backends.{database}','OPTIONS': {opening_brace}'read_default_file': '/path/to/{database}.cnf',{closing_brace},{closing_brace}{closing_brace}"
+    DATABASES = DATABASES.format(database=database, opening_brace='{', closing_brace='}')
     cmd = f"""
     mkdir -p {parent_dir}
     cd {parent_dir}
     echo "db_name={db_name} db_type={database}" > README.md
-    python3 -m venv venv
-    . venv/bin/activate
-    pip install django djangorestframework
+    . ../venv/bin/activate
+    pip freeze > requirements.txt
     django-admin startproject backend
     ng new frontend --defaults --skip-install=true
     """
     subprocess.check_output(cmd, shell=True)
-    venv_path = parent_dir / 'venv'
-    shutil.rmtree(venv_path)
+    with open(parent_dir / 'backend' / 'backend' / 'settings.py', 'a') as f:
+        f.write(f"DATABASES = {DATABASES}")
     zip = shutil.make_archive(proj_name, 'zip', f'{parent_dir}')
     shutil.rmtree(f'{parent_dir}')
     return pathlib.Path(zip)
